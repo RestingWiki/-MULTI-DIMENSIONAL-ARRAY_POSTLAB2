119dea836aa94d24f82c6195dfb5f2b83d0e43c8
RestingWiki <126898951+RestingWiki@users.noreply.github.com>
2023-03-20 20:57:19 +0700

    Setup

--- Project4/Source.cpp
+++ Project4/Source.cpp
@@ -4,12 +4,7 @@ using namespace std;
 
 
 
-int specialCells(int arr[][1000], int row, int col);
-int rowSum(int arr[][1000], const int& row, const int& col);
-int colSum(int arr[][1000], const int& row, const int& col);
-bool isPrime(int n);
-
-
+int subMatrix(int arr[][1000], int row, int col);
 int main() {
 	int arr[][1000] = { {37,53,74},{12,37,60},{98,13,15} };
 	cout << specialCells(arr, 3, 3);
@@ -18,62 +13,6 @@ int main() {
 }
 
 
-int specialCells(int arr[][1000],  int row,  int col) {
-	int rSum;
-	int cSum;
-	int HN3 = 0;
-		for (int i = 0; i < row; i++)
-		{
-			for (int j = 0; j < col; j++)
-			{
-				rSum = rowSum(arr,i,col);
-				cout << "ROW SUM = " << rSum << endl;
-				cSum = colSum(arr,row,j);
-				cout << "COL SUM = " << cSum << endl;
-
-				if (isPrime(rSum) && isPrime(cSum))
-					HN3++;
-
-			}
-		}
-
-
-
-		return HN3;
-}
-int rowSum(int arr[][1000], const int& row, const int& col) {
-	int n = 0;
-	for (int i = 0; i < col; i++)
-	{
-		cout << arr[row][i] << endl;
-		n += arr[row][i];
-	}
-	return n;
-}
-int colSum(int arr[][1000], const int& row, const int& col) {
-	int n = 0;
-	for (int i = 0; i < row; i++)
-	{
-		cout << arr[i][col];
-		n += arr[i][col];
-	}
-	return n;
-}
-bool isPrime(int n) {
-	if (n <= 1)
-		return false;
-	else if (n == 2)
-		return true;
-	else
-	{
-		for (int i = 2; i <= n / 2; i++)
-		{
-			if (n % i == 0)
-				return false;
-		}
-
-
-		return true;
-	}
+int subMatrix(int arr[][1000], int row, int col) {
 
 }
